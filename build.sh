2e265d92b0d3c63ff911798a5642348ba6104897
tangsan.xjb <tangsan.xjb@taobao.com>
2013-09-12 10:22:57 +0800

    post new blog

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 rm -fr compiled
-git add .
-git commit -m "update blog source"
-git push -u origin master
+#git add .
+#git commit -m "update blog source"
+#git push -u origin master
 ruhoh compile
+rm -fr ../tangsan.im/*
 cp -fr compiled/* ../tangsan.im
 cd ../tangsan.im
+echo tangsan.im>CNAME
 git add .
 git commit -m "post new blog"
-git remote add origin https://github.com/ecchanger/ecchanger.github.com.git
+git remote add origin https://github.com/ecchanger/ecchanger.github.io.git
 git push -u origin master
